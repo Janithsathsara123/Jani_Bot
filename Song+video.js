1d23b5134b4353227a538b6f46684b5991ba13ce
Janithsathsara123 <janithsathsara789@gmail.com>
2025-07-20 00:48:46 +0530

    Update Song+video.js

--- Song+video.js
+++ Song+video.js
@@ -42,3 +42,49 @@ await conn.sendmassge(from,{audio:{url:downloadurl}.mimetype:"audio/mpeg"},{quot
 Cosole.log(e)
 Reply(`${e}´)
 }                                                      
+
+
+
+
+
+###...........???.##
+
+cmd({
+    pattern: "video",
+    desc: "download.",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+(lq) return raply("please give me url or titel")
+Const search=await yts(q)
+const date = search.videos[0]:
+Cinst url = date. url
+
+Let desc = ' 
+😇 *JANI_MD_BOT video downlod*😇
+
+title ${date.title}
+Discription:${date.dicription}
+Time: ${date.timestp}
+ago: ${date.ago}
+Views:${date.views}
+Made by JANI_MD_BOT😇
+await conn.sendmessage(from,{image :{url:data.thumbnail },caption:desc},{quoted:mek}):
+
+//downlod video
+
+let down=await fg.yta(url)
+let   downlodurl=down.di_url
+
+
+//send video massge
+await conn.sendmassge(from,{video:{url:downloadurl}.mimetype:"video/mpeg"},{quoted:mak})
+
+
+
+ }catch(e){
+Cosole.log(e)
+Reply(`${e}´)
+}                                                      
