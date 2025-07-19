c90c9e24b38797ff21fb432649b6d576287eb41a
Janithsathsara123 <janithsathsara789@gmail.com>
2025-07-19 17:42:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ console.log('Bot connected to whatsapp ✅')
 
 let up = `JANI_MD_BOT connected successful ✅\n\nPREFIX: ${prefix}`;
 
-conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/900435c6d3157c98c3c88.jpg` }, caption: up })
+conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `IMG-20250719-WA0013.jpg` }, caption: up })
 
 }
 })
