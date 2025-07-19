67c5e04772e7c1ec3ddc5a2ed944ad18dcdfe02b
Janithsathsara123 <janithsathsara789@gmail.com>
2025-07-19 22:26:55 +0530

    Update Song+video.js

--- Song+video.js
+++ Song+video.js
@@ -2,17 +2,9 @@ const {cmd , commands} = require('../command')
 
 cmd({
     pattern: "song",
-    desc: "downlod.",
-    category: "video",
+    desc: "download.",
+    category: "download",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
-
-
