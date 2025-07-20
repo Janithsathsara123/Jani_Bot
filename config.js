20fd250fbd099011d1ec0d4cc343d92710b4ff50
Janithsathsara123 <janithsathsara789@gmail.com>
2025-07-20 11:42:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 Alive_img:process.evn.Alive_img||"IMG-20250719-WA0013.jpg",
-Alive_msg:process.evn.Alive_msg||"hello,alive yourname I am alive now".    
+Alive_msg:process.evn.Alive_msg||"hello,alive yourname I am alive now".  
+AUTO_READ_STATUS:process.evn.AUTO_READ_STATUS || "true",    
 };
