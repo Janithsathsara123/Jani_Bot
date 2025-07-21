327de7aebc8daea388a8d75c20798d505649c7d9
Janithsathsara123 <janithsathsara789@gmail.com>
2025-07-21 20:17:18 +0530

    Update System.js

--- System.js
+++ System.js
@@ -10,3 +10,5 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+
+}catch(e)    
