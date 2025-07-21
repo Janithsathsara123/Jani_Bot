b5488ff73327078a789d732457243bd53cc8500e
Janithsathsara123 <janithsathsara789@gmail.com>
2025-07-21 22:21:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,5 @@ SESSION_ID: process.env.SESSION_ID,
 Alive_img:process.evn.Alive_img||"IMG-20250719-WA0013.jpg",
 Alive_msg:process.evn.Alive_msg||"hello,alive yourname I am alive now".  
 AUTO_READ_STATUS:process.evn.AUTO_READ_STATUS || "true",    
+MODE:process.env.MODE||"public",
 };
